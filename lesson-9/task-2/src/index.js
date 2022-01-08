b9b4f1cd37de7c5f48048bbb9293143dfb2ec1b6
Denys <denzzlle171@gmail.com>
2022-01-08 15:52:56 +0300

    save 08/01/22 --02

---
+++ lesson-9/task-2/src/index.js
@@ -0,0 +1,28 @@
+import { initTodoListHandlers } from './list/todoList';
+import { renderTasks } from './list/render';
+import { getTasksList } from './list/tasksGateway';
+import { setItem } from './list/storage';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList().then((tasksList) => {
+    setItem('taskList', tasksList);
+    renderTasks();
+  });
+
+  initTodoListHandlers();
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
+};
+window.addEventListener('storage', onStorageChange);
+
+// {
+//           "edge": "17",
+//           "firefox": "60",
+//           "chrome": "67",
+//           "safari": "11.1"
+//         },
